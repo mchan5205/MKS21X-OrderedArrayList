20360990ae19481b9d07e700dc806cd02b444c64
Michael Chan <mchan10@stuy.edu>
2018-11-02 10:38:04 -0400

    added compare and add methods

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -6,4 +6,18 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList(int startingcapacity){
     super(startingcapacity);
   }
+  private int compare(T thing){
+    for (int i = 0; i < super.size(); i += 1){
+      if (super.get(i).compareTo(thing) < 0){
+        return i;
+      }
+    }
+  }
+  public boolean add(T value){
+    super.add(compare(value), value);
+    return true;
+  }
+  public void add(int y, T value){
+    super.add(compare(value),value);
+  }
 }
